aee2a1ba7137f6f7a1e67d05927e3e9c7e639c15
JamFox <github@jamfox.dev>
2024-03-22 16:59:45 +0200

    restructure, implement permutations

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::io;
 use std::io::Write;
 
 mod permutations;
-use permutations::{IP,FP,E,PF};
-//mod sboxes;
-//use sboxes::{S1,S2,S3,S4,S5,S6,S7,S8};
+use permutations::{
+    permute_u32, permute_u64, reverse_permute_u32, reverse_permute_u64, E, FP, IP, PC1, PC2, PF,
+};
+mod sboxes;
+//use sboxes::{};
 
 fn main() {
     println!("Welcome to the Rust Encryption/Decryption program!");
@@ -14,12 +16,14 @@ fn main() {
         println!("Choose an option:");
         println!("1. Encrypt");
         println!("2. Decrypt");
-        println!("3. Print Permutations");
+        println!("3. Print Debug stuff");
         println!("Press any other number to exit.");
         println!();
 
         let mut choice = String::new();
-        io::stdin().read_line(&mut choice).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
         let choice: u32 = match choice.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -30,17 +34,17 @@ fn main() {
 
         match choice {
             1 => {
-                let plaintext = get_input("Enter 64-bit plaintext: ");
-                let cipher_key = get_input("Enter 64-bit cipher key: ");
+                let plaintext = get_input("Enter plaintext as number: ");
+                let cipher_key = get_input("Enter cipher key as number: ");
                 encrypt(plaintext, cipher_key);
             }
             2 => {
-                let ciphertext = get_input("Enter 64-bit ciphertext: ");
-                let cipher_key = get_input("Enter 64-bit cipher key: ");
+                let ciphertext = get_input("Enter ciphertext as number: ");
+                let cipher_key = get_input("Enter cipher key as number: ");
                 decrypt(ciphertext, cipher_key);
             }
             3 => {
-                print_permutations();
+                print_debug();
             }
             _ => {
                 println!("Exiting program!");
@@ -50,92 +54,128 @@ fn main() {
     }
 }
 
-fn print_permutations() {
-    /* 
+fn print_debug() {
+    /*
      * Print the permutations for debugging purposes
-    */
+     */
     println!("Initial Permutation: {:?}", IP);
     println!("Final Permutation: {:?}", FP);
     println!("Expansion: {:?}", E);
     println!("Permutation in F: {:?}", PF);
+    println!("Permuted Choice One (PC1): {:?}", PC1);
+    println!("Permuted Choice Two (PC2): {:?}", PC2);
     println!();
 
     // Initialize the data array with consecutive integers starting from 1
-    let mut data: [usize; 64] = [0; 64];
+    let mut data: [u64; 64] = [0; 64];
     for (index, element) in data.iter_mut().enumerate() {
-        *element = index + 1;
+        *element = (index + 1) as u64;
     }
 
-    let permuted_data: [usize; 64] = permute(&IP, &data);
+    //let permuted_data: [u64; 64] = permute(&IP, &data);
 
-    reverse_permute(&IP, &permuted_data);
+    //reverse_permute(&IP, &permuted_data);
 }
 
-fn get_input(prompt: &str) -> String {
+fn get_input(prompt: &str) -> u64 {
     loop {
+        let mut input = String::new();
         print!("{}", prompt);
         let _ = io::stdout().flush();
-        
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
 
-        let trimmed_input = input.trim();
-        
-        // Each char is 4 bytes, we want the input to be 64 bits long, so input should be 2 characters long
-        if trimmed_input.len() < 2 {
-            println!("Input should be at least 2 characters long. Please try again.");
-        } else {
-            return trimmed_input[..2].to_string(); // Take first 2 characters if input is longer
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input = input.trim();
+
+        // Attempt to parse the input as a hexadecimal u64
+        match u64::from_str_radix(input, 16) {
+            Ok(parsed) => {
+                println!("Your value: {}", parsed);
+                return input.parse().unwrap();
+            }
+            Err(_) => {
+                println!("Invalid input. Please enter a valid hexadecimal number.");
+            }
         }
     }
 }
 
-fn permute(permutation_table: &[usize; 64], data: &[usize; 64]) -> [usize; 64] {
-    let mut permuted_data: [usize; 64] = [0; 64];
-    for i in 0..64 {
-        // Data element i goes to the position specified in permutation table
-        // For example if permutation_table[i] = 58, then data[i] goes to position 58 - 1 = 57 in permuted_data
-        permuted_data[permutation_table[i] - 1] = data[i];
-    }
-
-    // Print the original and permuted data
-    println!("Original Data: {:?}", data);
-    println!("Permuted Data: {:?}", permuted_data);
-    println!();
-
-    permuted_data
+fn split_u64_to_u32_halves(input: u64) -> (u32, u32) {
+    // shifts the bits of the input to the right by 32 positions, isolating the upper 32 bits
+    let left_half = (input >> 32) as u32;
+    println!("Upper half: {:032b}", left_half);
+    // simply casts the u64 input to a u32, isolating the lower 32 bits
+    let right_half = input as u32;
+    println!("Lower half: {:032b}", right_half);
+    (left_half, right_half)
 }
 
-fn reverse_permute(permutation_table: &[usize; 64], permuted_data: &[usize; 64]) -> [usize; 64] {
-    let mut data: [usize; 64] = [0; 64];
-    for i in 0..64 {
-        // Permuted data element i goes back to the position specified in permutation table
-        // For example if permutation_table[i] = 58, then permuted_data[i] goes to position 58 - 1 = 57 in data
-        data[i] = permuted_data[permutation_table[i] - 1];
+fn key_generation(key: u64) -> [u64; 16] {
+    let mut round_keys: [u64; 16] = [0; 16]; // Initialize an array to store round keys.
+
+    // Step 1: Permuted Choice One (PC1)
+    let mut permuted_choice_one: u64 = permute_u64(&PC1, key);
+
+    // Step 2: Split into left and right halves
+    let (mut left, mut right) = split_u64_to_u32_halves(permuted_choice_one);
+    // TODO: convert to halves to 28 bits each?
+
+    // Step 3: 16 rounds of key generation
+    for i in 0..16 {
+        println!("Keygen Round {} ", i + 1);
+        // Step 3.1 Left shift for the round and mask
+
+        // Step 3.2 Permuted Choice Two (PC2)
+        let combined: u64 = 1; // placeholder for testing
+        let mut round_key: u64 = permute_u64(&PC2, combined);
+
+        // Step3.3 Store the round key
+
     }
 
-    // Print the original and reverse-permuted data
-    println!("Permuted Data: {:?}", permuted_data);
-    println!("Reversed Permuted Data: {:?}", data);
-    println!();
-
-    data
+    round_keys // Return the array of round keys.
 }
 
-fn encrypt(plaintext: String, cipher_key: String) {
+fn encrypt(plaintext: u64, cipher_key: u64) {
     // Placeholder for encryption function
+    println!("--- String represantation ---");
     println!("Encrypting: {} with key: {}", plaintext, cipher_key);
-    let plaintext_byte_string: &[u8] = plaintext.as_bytes();
-    println!("Plaintext Byte String: {:?}", plaintext_byte_string);
-    let cipher_key_byte_string: &[u8] = cipher_key.as_bytes();
-    println!("Key Byte String: {:?}", cipher_key_byte_string);
+
+    // Step 1: Initial Permutation
+    let permuted: u64 = permute_u64(&IP, plaintext);
+
+    // Step 2: Split into left and right halves
+    let (mut left, mut right) = split_u64_to_u32_halves(permuted);
+
+    // Step 3: Key generation
+    let round_keys: [u64; 16] = key_generation(cipher_key);
+
+    // Step 4: 16 rounds
+    for i in 0..16 {
+        println!("Round {} ", i + 1);
+        let prev_right: u32 = right;
+        // Step 4.1: Expansion
+
+        // Step 4.2: XOR with round key
+
+        // Step 4.3: S-box substitution
+        let sbox_result: u32 = 1; // placeholder for testing because sboxes not implemented yet
+
+        // Step 4.4: Permutation
+        let permuted_s_box: u32 = permute_u32(&PF, sbox_result);
+
+        // Step 4.5: XOR with left half and update halves
+        right = left ^ permuted_s_box;
+        left = prev_right;
+    }
+
+    // Step 5: Final Permutation
+    let final_permuted: u64 = permute_u64(&FP, (left as u64) << 32 | right as u64);
+    println!("Final permuted: {:064b}", final_permuted);
 }
 
-fn decrypt(ciphertext: String, cipher_key: String) {
-    // Placeholder for decryption function
-    println!("Decrypting: {} with key: {}", ciphertext, cipher_key);
-    let ciphertext_byte_string: &[u8] = ciphertext.as_bytes();
-    println!("Ciphertext Byte String: {:?}", ciphertext_byte_string);
-    let cipher_key_byte_string: &[u8] = cipher_key.as_bytes();
-    println!("Key Byte String: {:?}", cipher_key_byte_string);
+fn decrypt(ciphertext: u64, cipher_key: u64) {
+    println!("Not implemented");
 }
--- src/permutations.rs
+++ src/permutations.rs
@@ -1,14 +1,96 @@
-//Permutations
+// Permutations
 // According to standard: https://en.wikipedia.org/wiki/DES_supplementary_material
 
-//Initial permutation
-pub const IP: [usize; 64] = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7];
+// Initial permutation
+pub const IP: [u64; 64] = [
+    58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6,
+    64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61,
+    53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7,
+];
 
-//Final permutation
-pub const FP: [usize; 64] = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25];
+// Final permutation
+// Inverse of the Initial Permutation
+pub const FP: [u64; 64] = [
+    40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25,
+];
 
-//Expansion
-pub const E: [usize; 48] = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1];
+// Expansion
+pub const E: [u64; 48] = [
+    32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18,
+    19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1,
+];
 
-//Permutation in F
-pub const PF: [usize; 32] = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25];
+// Permutation in F
+pub const PF: [u64; 32] = [
+    16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19,
+    13, 30, 6, 22, 11, 4, 25,
+];
+
+// Permuted Choice One (PC1)
+// B64 to B56
+pub const PC1: [u64; 56] = [
+    57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60,
+    52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29,
+    21, 13, 5, 28, 20, 12, 4,
+];
+
+// Permuted Choice Two (PC2)
+// B56 to B48
+pub const PC2: [u64; 48] = [
+    14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52,
+    31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32,
+];
+
+pub fn permute_u64(permutation_table: &[u64], data: u64) -> u64 {
+    let mut permuted_data: u64 = 0;
+    for i in 0..permutation_table.len() {
+        // Get the i-th bit from the original data
+        let data_bit = (data >> i) & 1;
+        // Set the i-th bit in the permuted data according to the permutation table
+        permuted_data |= data_bit << (permutation_table[i] - 1);
+    }
+    println!("Original Data: {:064b}", data);
+    println!("Permuted Data: {:064b}", permuted_data);
+    permuted_data
+}
+
+pub fn permute_u32(permutation_table: &[u64], data: u32) -> u32 {
+    let mut permuted_data: u32 = 0;
+    for i in 0..permutation_table.len() {
+        // Get the i-th bit from the original data
+        let data_bit = (data >> i) & 1;
+        // Set the i-th bit in the permuted data according to the permutation table
+        permuted_data |= data_bit << (permutation_table[i] - 1) as u32;
+    }
+    println!("Original Data: {:032b}", data);
+    println!("Permuted Data: {:032b}", permuted_data);
+    permuted_data
+}
+
+pub fn reverse_permute_u64(permutation_table: &[u64], permuted_data: u64) -> u64 {
+    let mut data: u64 = 0;
+    for i in 0..permutation_table.len() {
+        // Get the i-th bit from the permuted data according to the permutation table
+        let data_bit = (permuted_data >> (permutation_table[i] - 1)) & 1;
+        // Set the i-th bit in the original data
+        data |= data_bit << i;
+    }
+    println!("Original Permuted Data: {:064b}", permuted_data);
+    println!("Reverse Permuted Data: {:064b}", data);
+    data
+}
+
+pub fn reverse_permute_u32(permutation_table: &[u64], permuted_data: u32) -> u32 {
+    let mut data: u32 = 0;
+    for i in 0..permutation_table.len() {
+        // Get the i-th bit from the permuted data according to the permutation table
+        let data_bit = (permuted_data >> (permutation_table[i] - 1) as u32) & 1;
+        // Set the i-th bit in the original data
+        data |= data_bit << i;
+    }
+    println!("Original Permuted Data: {:032b}", permuted_data);
+    println!("Reverse Permuted Data: {:032b}", data);
+    data
+}
